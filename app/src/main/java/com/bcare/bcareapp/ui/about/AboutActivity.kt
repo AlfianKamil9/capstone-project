2b4a532b80c4888ddb0ae3b295506ed30582685b
MuliaSahpira <muliasahpira@gmail.com>
2023-12-02 16:47:03 +0700

    UI Profile, About App, Detail Artikel

---
+++ app/src/main/java/com/bcare/bcareapp/ui/about/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.bcare.bcareapp.ui.about
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bcare.bcareapp.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
