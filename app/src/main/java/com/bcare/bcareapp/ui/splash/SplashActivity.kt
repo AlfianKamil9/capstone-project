b74246321707924ecb45ba6b0f437759fe9c8719
Muhammad Dwiky Alfira <dwikyalfira1@gmail.com>
2023-12-03 22:24:09 +0700

    Change AndroidManifest.xml intent-filter to SplashActivity.

--- app/src/main/java/com/bcare/bcareapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bcare/bcareapp/ui/splash/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.bcare.bcareapp.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.bcare.bcareapp.R
+import com.bcare.bcareapp.ui.main.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1950)
     }
 }
