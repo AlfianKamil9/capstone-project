2fde0e1a3d1d847fd447bf1ec3dda59900c86a29
AlfianKamil9 <rifqialfiansyahk@gmail.com>
2023-12-07 23:03:42 +0700

    updateses

--- index.js
+++ index.js
@@ -15,43 +15,51 @@ const { loadModel, predict, loadModelImage, predictImage } = require('./ml'); //
   // Middleware untuk mengolah JSON
   const upload = multer();
   app.use(bodyParser.json());
+
+  //TOKEN
+  const keyToken = 'capstone-project-machine-learning';
+
+  // set Token
+  const token = (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization.split(' ')[1];
+    if (!authorization || token != keyToken) {
+      return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+    }
+    next();
+  };
+
+  app.use(token);
+
   app.post('/predict-form', async (req, res) => {
-    const { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 } = req.body;
-    const input = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10].map((e) => {
-      switch (e.toLowerCase()) {
-        case 'a':
-          return 0;
-        case 'b':
-          return 1;
-        case 'c':
-          return 2;
-        default:
-          return 3;
-      }
-    });
+    const { input } = req.body;
 
     // Lakukan prediksi menggunakan fungsi predict
     const result = await predict(model, input);
     console.log(result);
     console.log(result[0]);
-    return res.json({
-      code: 200,
-      message: 'Complete predict form',
-      data: kondisi[result[0]],
-    });
+    return res
+      .json({
+        code: 200,
+        message: 'Complete predict form',
+        data: kondisi[result[0]],
+      })
+      .status(201);
   });
-  //
+
   app.post('/predict-image', upload.single('file'), async (req, res) => {
     const image = req.file.buffer;
     console.log(image);
     const result = await predictImage(modelImage, image);
     console.log(result);
     console.log(result[0]);
-    return res.json({
-      code: 200,
-      message: 'Complete predict image',
-      data: kondisi[result[0]],
-    });
+    return res
+      .json({
+        code: 200,
+        message: 'Complete predict image',
+        data: kondisi[result[0]],
+      })
+      .status(201);
   });
 
   // Jalankan server pada port tertentu
